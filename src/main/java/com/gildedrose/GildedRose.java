8cbca73b0d6a9d95e8c77b6f860143e9849825bd
Tim Boeckstaens <boeckstaenstim@gmail.com>
2018-11-25 14:00:37 +0100

    introduce strategy

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -9,51 +9,66 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            updateQualityOf(items[i]);
+            Item item = items[i];
+            getQualityRuleFor(item.name).updateQualityOf(item);
         }
     }
 
-    private void updateQualityOf(Item item) {
-        if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            return;
+    @FunctionalInterface
+    private interface QualityRule {
+        void updateQualityOf(Item item);
+    }
+
+    private QualityRule getQualityRuleFor(String name) {
+        if (name.equals("Sulfuras, Hand of Ragnaros")) {
+            return item -> {
+            };
         }
-        item.sellIn = item.sellIn - 1;
-        if (item.name.equals("Aged Brie")) {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1;
-            }
-            if(item.sellIn < 0) {
+        if (name.equals("Aged Brie")) {
+            return item -> {
+                item.sellIn = item.sellIn - 1;
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
                 }
-            }
-        } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1;
-                if (item.sellIn < 10) {
+                if (item.sellIn < 0) {
                     if (item.quality < 50) {
                         item.quality = item.quality + 1;
                     }
                 }
+            };
+        } else if (name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            return item -> {
+                item.sellIn = item.sellIn - 1;
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                    if (item.sellIn < 10) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
+                    }
 
-                if (item.sellIn < 5) {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
+                    if (item.sellIn < 5) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
                     }
                 }
-            }
-            if(item.sellIn < 0) {
-                item.quality = item.quality - item.quality;
-            }
+                if (item.sellIn < 0) {
+                    item.quality = item.quality - item.quality;
+                }
+            };
         } else {
-            if (item.quality > 0) {
-                item.quality = item.quality - 1;
-            }
-            if(item.sellIn < 0) {
+            return item -> {
+                item.sellIn = item.sellIn - 1;
                 if (item.quality > 0) {
                     item.quality = item.quality - 1;
                 }
-            }
+                if (item.sellIn < 0) {
+                    if (item.quality > 0) {
+                        item.quality = item.quality - 1;
+                    }
+                }
+            };
         }
     }
 }
