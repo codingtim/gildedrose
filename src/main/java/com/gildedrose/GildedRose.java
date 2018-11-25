d5b3939eca120b7a6e510ba2911088de34973427
Tim Boeckstaens <boeckstaenstim@gmail.com>
2018-11-25 13:41:52 +0100

    consolidate duplicate conditional expressions

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -22,6 +22,11 @@ class GildedRose {
             if (item.quality < 50) {
                 item.quality = item.quality + 1;
             }
+            if(item.sellIn < 0) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                }
+            }
         } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
             if (item.quality < 50) {
                 item.quality = item.quality + 1;
@@ -37,19 +42,14 @@ class GildedRose {
                     }
                 }
             }
+            if(item.sellIn < 0) {
+                item.quality = item.quality - item.quality;
+            }
         } else {
             if (item.quality > 0) {
                 item.quality = item.quality - 1;
             }
-        }
-        if (item.sellIn < 0) {
-            if (item.name.equals("Aged Brie")) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
-            } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                item.quality = item.quality - item.quality;
-            } else {
+            if(item.sellIn < 0) {
                 if (item.quality > 0) {
                     item.quality = item.quality - 1;
                 }
