1c622fcc24f7fb041b9358c9d295909c980b47da
Tim Boeckstaens <boeckstaenstim@gmail.com>
2018-11-25 13:31:17 +0100

    add guard to short circuit
    This way the rest of the branches become easier.

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -14,6 +14,9 @@ class GildedRose {
     }
 
     private void updateQualityOf(Item item) {
+        if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            return;
+        }
         if (item.name.equals("Aged Brie")) {
             if (item.quality < 50) {
                 item.quality = item.quality + 1;
@@ -33,15 +36,13 @@ class GildedRose {
                     }
                 }
             }
-        } else if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+        } else {
             if (item.quality > 0) {
                 item.quality = item.quality - 1;
             }
         }
 
-        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            item.sellIn = item.sellIn - 1;
-        }
+        item.sellIn = item.sellIn - 1;
 
         if (item.sellIn < 0) {
             if (item.name.equals("Aged Brie")) {
@@ -50,7 +51,7 @@ class GildedRose {
                 }
             } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 item.quality = item.quality - item.quality;
-            } else if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            } else {
                 if (item.quality > 0) {
                     item.quality = item.quality - 1;
                 }
