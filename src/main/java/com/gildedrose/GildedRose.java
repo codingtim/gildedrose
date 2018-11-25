8d7f3862c36701a2fcbb3d685cc8940bca0dfd6d
Tim Boeckstaens <boeckstaenstim@gmail.com>
2018-11-25 14:05:32 +0100

    extract classes
    This splits the choice of strategy away from the logic.

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -14,61 +14,82 @@ class GildedRose {
         }
     }
 
+    private QualityRule getQualityRuleFor(String name) {
+        if (name.equals("Sulfuras, Hand of Ragnaros")) {
+            return new LegendaryItemRule();
+        } else if (name.equals("Aged Brie")) {
+            return new AgingItemRule();
+        } else if (name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            return new PassRule();
+        } else {
+            return new NormalItemRule();
+        }
+    }
+
     @FunctionalInterface
     private interface QualityRule {
         void updateQualityOf(Item item);
     }
 
-    private QualityRule getQualityRuleFor(String name) {
-        if (name.equals("Sulfuras, Hand of Ragnaros")) {
-            return item -> {
-            };
+    private static class LegendaryItemRule implements QualityRule {
+        @Override
+        public void updateQualityOf(Item item) {
+            //legendary items do not change in quality
         }
-        if (name.equals("Aged Brie")) {
-            return item -> {
-                item.sellIn = item.sellIn - 1;
+    }
+
+    private static class NormalItemRule implements QualityRule {
+        @Override
+        public void updateQualityOf(Item item) {
+            item.sellIn = item.sellIn - 1;
+            if (item.quality > 0) {
+                item.quality = item.quality - 1;
+            }
+            if(item.sellIn < 0) {
+                if (item.quality > 0) {
+                    item.quality = item.quality - 1;
+                }
+            }
+        }
+    }
+
+    private static class AgingItemRule implements QualityRule {
+        @Override
+        public void updateQualityOf(Item item) {
+            item.sellIn = item.sellIn - 1;
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
+            }
+            if (item.sellIn < 0) {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
                 }
-                if (item.sellIn < 0) {
+            }
+        }
+    }
+
+    private static class PassRule implements QualityRule {
+        @Override
+        public void updateQualityOf(Item item) {
+            item.sellIn = item.sellIn - 1;
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
+                if (item.sellIn < 10) {
                     if (item.quality < 50) {
                         item.quality = item.quality + 1;
                     }
                 }
-            };
-        } else if (name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            return item -> {
-                item.sellIn = item.sellIn - 1;
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                    if (item.sellIn < 10) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
 
-                    if (item.sellIn < 5) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
+                if (item.sellIn < 5) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
                     }
                 }
-                if (item.sellIn < 0) {
-                    item.quality = item.quality - item.quality;
-                }
-            };
-        } else {
-            return item -> {
-                item.sellIn = item.sellIn - 1;
-                if (item.quality > 0) {
-                    item.quality = item.quality - 1;
-                }
-                if (item.sellIn < 0) {
-                    if (item.quality > 0) {
-                        item.quality = item.quality - 1;
-                    }
-                }
-            };
+            }
+            if (item.sellIn < 0) {
+                item.quality = item.quality - item.quality;
+            }
         }
+
     }
 }
