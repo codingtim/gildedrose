402df20295e4ff5a44757816084e3c311b1fb8d7
Tim Boeckstaens <boeckstaenstim@gmail.com>
2018-11-25 13:20:30 +0100

    invert if condition

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -39,7 +39,11 @@ class GildedRose {
             }
 
             if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
+                if (items[i].name.equals("Aged Brie")) {
+                    if (items[i].quality < 50) {
+                        items[i].quality = items[i].quality + 1;
+                    }
+                } else {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].quality > 0) {
                             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
@@ -48,10 +52,6 @@ class GildedRose {
                         }
                     } else {
                         items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
                     }
                 }
             }
